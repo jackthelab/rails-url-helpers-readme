b01d9a03c55c478c5895cef2d55aef501493e75e
Jacob Hampton <jhampton792@gmail.com>
2021-02-04 09:45:56 -0600

    commit tree 67eb8e8514eacd5e26784138dd8c2e94367f8516

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts, only: [:index, :show]
+  # resources :posts, only: [:index, :show]
+  get '/posts', to: 'posts#index'
+  get '/posts/:id', to: 'posts#show', as: :post
 end
